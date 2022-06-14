cd36d0f81ca4688a8ea216cca276b638d348f9a4
tykim <tykim@selper.co.kr>
2022-06-14 22:34:38 +0900

    레이아웃 변경

--- config.h
+++ config.h
@@ -1,4 +1,20 @@
 #pragma once
 
 #define AUTO_SHIFT_TIMEOUT 150
+
 #define MK_3_SPEED
+
+#define MK_C_OFFSET_0 4
+#define MK_C_INTERVAL_0 16
+#define MK_W_OFFSET_0 1
+#define MK_W_INTERVAL_0 60
+
+#define MK_C_OFFSET_1 16
+#define MK_C_INTERVAL_1 16
+#define MK_W_OFFSET_1 1
+#define MK_W_INTERVAL_1 30
+
+#define MK_C_OFFSET_2 32
+#define MK_C_INTERVAL_2 16
+#define MK_W_OFFSET_2 1
+#define MK_W_INTERVAL_2 10
